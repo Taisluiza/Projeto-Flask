fc2cb320c7548378bcf6c148c385e83f7c92409d
TaisLuiza <taissalissousa@gmail.com>
2026-09-13 16:19:22 -0300

    atualizando o deletar e finalizando o CRUD

--- app/routes/main.py
+++ app/routes/main.py
@@ -86,14 +86,41 @@ def get_product_by_id(product_id):
     return jsonify({"message":f"Esta é a rota de visualizacao do detalhe do id do produto {product_id}"})
 
 # O sistema deve permitir a atualizacao de um unico produto e produto existente
-@main_bp.route('/product/<int:product_id>', methods=['PUT'])
-def update_product(product_id):
-    return jsonify({"message":f"Esta é a rota de atualizacao do produto com o id {product_id}"})
+@main_bp.route('/product/<string:product_id>', methods=['PUT'])
+@token_required
+def update_product(token, product_id):
+    try: 
+       oid= ObjectId(product_id)
+       update_data = UpdateProduct(**request.get_json())
+
+    except ValidationError as e:
+       return jsonify({"error": e.errors()})
+
+    update_result = db.products.update_one(
+       {"_id": oid},
+       {"$set": update_data.model_dump(exclude_unset=True)}
+    )
+
+    if update_result.matched_count == 0:
+       return jsonify({"error": "Produto não encontrado"}),404
+
+    updated_product = db.products.find_one({"_id":oid})
+    return jsonify(ProductDBModel(**updated_product).model_dump(by_alias=True, exclude=None))
 
 # O sistema deve permitir a delecao de um unico produto e produto existente
-@main_bp.route('/product/<int:product_id>', methods=['DELETE'])
-def delete_product(product_id):
-    return jsonify({"message":f"Esta é a rota de deleção do produto com o id {product_id}"})
+@main_bp.route('/product/<string:product_id>', methods=['DELETE'])
+@token_required
+def delete_product(token, product_id):
+    try:
+        oid = ObjectId(product_id)
+    except Exception:
+       return jsonify({"error":"id do produto invalido"}),400
+
+    delete_product = db.products.delete_one({"_id": oid})
+
+    if delete_product.deleted_count == 0:
+       return jsonify({"error":"Produto não foi encontrado"}),400
+    return "", 204
 
 # O sistema deve permitir a importacao de vendas através de um arquivo
 @main_bp.route('/sales/upload', methods=['POST'])
